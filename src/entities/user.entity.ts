311946e94339f7df32e2abe66cd43451c332bd5f
george-asiedu <asiedug41@gmail.com>
2025-05-01 23:51:01 +0000

    user entity schema defined

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { UserRole } from 'src/model/role.enum';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  public id!: string;
+
+  @Column()
+  public name!: string;
+
+  @Column({ type: 'varchar', length: 15, nullable: true })
+  public phone!: string;
+
+  @Column({ type: 'date', nullable: true })
+  public dob!: string;
+
+  @Column({ unique: true })
+  public email!: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.User,
+  })
+  public role!: UserRole;
+
+  @Column()
+  public password!: string;
+
+  @Column({ nullable: true })
+  public refreshToken!: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  code!: string | null;
+
+  @Column({ default: false })
+  isVerified!: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ type: 'timestamp', nullable: true })
+  deletedAt!: Date | null;
+
+  public static async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+  }
+}
