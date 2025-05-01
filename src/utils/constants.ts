1a00d029deac1ac0fe50357947b1c99f2a8b4544
george-asiedu <asiedug41@gmail.com>
2025-05-01 23:50:12 +0000

    constants file created

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const constants = {
+  swaggerDocsTitle: 'User Authentication API Documentation',
+  swaggerDocsDescription: 'REST API for managing the User authentication',
+  swaggerDocsVersion: '1.0.0',
+  swaggerDocsPath: 'api-docs',
+  productionUrl: '',
+  localUrl: 'http://localhost:',
+  globalPrefix: 'api',
+  ROLES_KEY: 'roles',
+  UnauthorizedError: 'Unauthorized user access',
+}
