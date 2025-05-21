dd560ed834072c3f1deb4348e0e81cb6376b8815
george-asiedu <asiedug41@gmail.com>
2025-05-21 20:08:57 +0000

    db config updated

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -5,15 +5,16 @@ export const pgConfig = (
   configService: ConfigService,
 ): PostgresConnectionOptions => ({
   type: 'postgres',
-  url: configService.get<string>('DB_CONNECTION'),
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize:
     configService.get<string>('NODE_ENV') !== 'production'
       ? configService.get<boolean>('DB_SYNC', true)
       : false,
-  extra: {
-    charset: 'utf8mb4_unicode_ci',
-  },
   ssl: {
     rejectUnauthorized: false,
   },
