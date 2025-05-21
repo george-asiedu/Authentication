78860d03b0bdd96ce7cce1d26ab94fae42ad537d
george-asiedu <asiedug41@gmail.com>
2025-05-21 20:07:52 +0000

    updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Authenticating user...';
+    return 'Restful API for authentication';
   }
 }
