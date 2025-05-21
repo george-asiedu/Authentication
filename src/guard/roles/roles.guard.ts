29d37a6c0643b18616ab9987abadc97d01cb4522
george-asiedu <asiedug41@gmail.com>
2025-05-21 20:09:29 +0000

    roles guard implemented

--- src/guard/roles/roles.guard.ts
+++ src/guard/roles/roles.guard.ts
@@ -1,5 +1,10 @@
 /* eslint-disable */
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { constants } from '../../utils/constants';
@@ -8,23 +13,23 @@ import { UserRole } from 'src/model/role.enum';
 @Injectable()
 export class RolesGuard implements CanActivate {
   private ROLES_KEY: string = constants.ROLES_KEY;
-  private unauthorizedError: string = constants.UnauthorizedError
+  private unauthorizedError: string = constants.UnauthorizedError;
 
   constructor(private reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(this.ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      this.ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
 
     if (!requiredRoles) {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    const hasRole = requiredRoles.some(
-      (role) => user.role?.includes(role)
-    );
+    const hasRole = requiredRoles.some((role) => user.role?.includes(role));
 
     if (!hasRole) {
       throw new UnauthorizedException(this.unauthorizedError);
