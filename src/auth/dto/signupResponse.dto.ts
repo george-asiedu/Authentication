370ea670a528a05945c0ad1fcf8c899fc56e1865
george-asiedu <asiedug41@gmail.com>
2025-05-21 20:08:45 +0000

    required options removed

--- src/auth/dto/signupResponse.dto.ts
+++ src/auth/dto/signupResponse.dto.ts
@@ -3,20 +3,23 @@ import { UserRole } from 'src/model/role.enum';
 
 export class SignupResponseDto {
   @ApiProperty({ example: 'd290f1ee-6c54-4b01-90e6-d701748f0851' })
-  id!: string;
+  id: string;
 
   @ApiProperty({ example: '123456', nullable: true })
-  code!: string | null;
+  code: string | null;
 
   @ApiProperty({ example: 'johndoe@test.com' })
-  email!: string;
+  email: string;
 
   @ApiProperty({ example: '0235678569' })
-  phone!: string;
+  phone: string;
 
   @ApiProperty({ example: 'John Doe' })
-  name!: string;
+  name: string;
+
+  @ApiProperty({ example: '25-10-2000' })
+  dob: string;
 
   @ApiProperty({ enum: UserRole, example: UserRole.User })
-  role!: UserRole;
+  role: UserRole;
 }
